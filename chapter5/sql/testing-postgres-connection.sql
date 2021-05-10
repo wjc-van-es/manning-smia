5c24df2ee45ba54d052c351031374524bc6ee4b5
Willem van Es <wjc.van.es@gmail.com>
2021-05-10 23:58:38 +0200

    Documentation about fixing docker login, volume and image cleanup.

--- chapter5/sql/testing-postgres-connection.sql
+++ chapter5/sql/testing-postgres-connection.sql
@@ -1,14 +1,18 @@
 select * 
 from licenses ls 
 inner join organizations os 
-on ls.organization_id = os.organization_id;
+on ls.organization_id = os.organization_id
+-- WHERE os.organization_id = 'd898a142-de44-466c-8c88-9ceb2c2429d3'
+;
 
 -- comment is a keyword thereofore changing it is tricky
-update licenses set "comment" = 'No comments' where license_id = 'f2a9c9d4-d2c0-44fa-97fe-724d77173c62';
-update licenses set "comment" = 'Not interested.' where license_id = '279709ff-e6d5-4a54-8b55-a5c37542025b';
+update licenses set "comment" = 'No comments added on Monday 10-05-2021' where license_id = 'f2a9c9d4-d2c0-44fa-97fe-724d77173c62';
+update licenses set "comment" = 'Not interested added on Monday 10-05-2021.' where license_id = '279709ff-e6d5-4a54-8b55-a5c37542025b';
 update licenses set description = 'Hugely Improved Software Product' where license_id = 'f2a9c9d4-d2c0-44fa-97fe-724d77173c62';
-update licenses set description = 'Hihgly Overrated Software Product' where license_id = '279709ff-e6d5-4a54-8b55-a5c37542025b';
+update licenses set description = 'Highly Overrated Software Product' where license_id = '279709ff-e6d5-4a54-8b55-a5c37542025b';
 commit;
 
 insert into licenses (license_id, organization_id, description, product_name, license_type, "comment") 
-values ('whatever-licence-id', 'd898a142-de44-466c-8c88-9ceb2c2429d3', 'inserted licence', 'VEA-STOCK', 'rubbish', 'Will I be seen?')
+values ('whatever-licence-id', 'd898a142-de44-466c-8c88-9ceb2c2429d3', 'inserted licence', 'VEA-STOCK', 'rubbish', 'Will I be seen?');
+insert into licenses (license_id, organization_id, description, product_name, license_type, "comment") 
+values ('whatever-licence-id-2', 'd898a142-de44-466c-8c88-9ceb2c2429d3', '2nd inserted licence', 'VEA-STOCK upgrade', 'still rubbish', 'Added on Monday 10-05-2021');
