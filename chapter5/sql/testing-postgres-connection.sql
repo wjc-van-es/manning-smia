ea8f3747653df3152a580470074c3cf3327571f9
Willem van Es <wjc.van.es@gmail.com>
2021-09-07 23:15:39 +0200

    updating documentation.

--- chapter5/sql/testing-postgres-connection.sql
+++ chapter5/sql/testing-postgres-connection.sql
@@ -5,7 +5,7 @@ on ls.organization_id = os.organization_id
 -- WHERE os.organization_id = 'd898a142-de44-466c-8c88-9ceb2c2429d3'
 order by ls.organization_id, ls.license_id ;
 
--- comment is a keyword thereofore changing it is tricky
+-- comment is a keyword thereofore changing it is tricky use double quotes
 update licenses set "comment" = 'No comments added on Monday 10-05-2021' where license_id = 'f2a9c9d4-d2c0-44fa-97fe-724d77173c62';
 update licenses set "comment" = 'Not interested added on Monday 10-05-2021.' where license_id = '279709ff-e6d5-4a54-8b55-a5c37542025b';
 update licenses set description = 'Hugely Improved Software Product' where license_id = 'f2a9c9d4-d2c0-44fa-97fe-724d77173c62';
